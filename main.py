74bf998a1ab5071a2ff93c4e96c04ed6ba6c81e8
Kevin <ankhanh2605@gmail.com>
2024-12-22 00:11:35 +0700

    update session analysis

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import logging
 from config.spark_config import get_spark_session
-from src.analytic_functions import calculate_dau_and_mau
+from src.analytic_functions import calculate_sessions, calculate_dau_and_mau
 from src.utils import read_csv, write_parquet
 
 # Configure logging
@@ -20,15 +20,24 @@ def main():
         logger.info("Starting DAU/MAU calculations.")
         dau_df, mau_df = calculate_dau_and_mau(df)
 
-        # Write data to parquet file
+        # Write DAU/MAU analysis data to parquet file
         write_parquet(dau_df, "output/daily_active_user_analysis.parquet")
         write_parquet(mau_df, "output/monthly_active_user_analysis.parquet")
+
+        # Perform session analysis
+        logger.info("Starting session analysis.")
+        session_df = calculate_sessions(df)
+
+        # Write Session analysis data to parquet file
+        write_parquet(session_df, "output/session_analysis.parquet")
+        logger.info("All tasks completed successfully.")
 
     except Exception as e:
         logger.error(f"An error occurred in the main workflow: {str(e)}", exc_info=True)
     finally:
         logger.info("Shutting down Spark session.")
-        spark.stop()
+        # spark.stop()
+        input()
 
 if __name__ == "__main__":
     main()
--- src/analytic_functions.py
+++ src/analytic_functions.py
@@ -1,5 +1,5 @@
 from pyspark.sql import DataFrame, Window
-from pyspark.sql.functions import col, to_date, countDistinct, year, month, concat_ws
+from pyspark.sql.functions import col, lag, unix_timestamp, from_unixtime, when, sum as _sum, min, max, count, avg, current_timestamp, concat, lit, to_date, countDistinct, year, month
 
 
 def calculate_dau_and_mau(df: DataFrame) -> DataFrame:
@@ -21,8 +21,23 @@ def calculate_dau_and_mau(df: DataFrame) -> DataFrame:
     # Calculate Daily Active User: Group by interaction_date and count distinct users
     dau_df = df.groupBy("interaction_date").agg(countDistinct("user_id").alias("dau"))
 
+    dau_df = dau_df.select(
+        col("interaction_date"),
+        col("dau"),
+        current_timestamp().alias("created_at"),
+        current_timestamp().alias("updated_at")
+    )
+
     # Calculate Monthly Active User: Group by year and month and count distinct users
     mau_df = df.groupBy("year", "month").agg(countDistinct("user_id").alias("mau"))
+
+    mau_df = mau_df.select(
+        col("year"),
+        col("month"),
+        col("mau"),
+        current_timestamp().alias("created_at"),
+        current_timestamp().alias("updated_at")
+    )
 
     # Show the result
     dau_df.show()
@@ -33,3 +48,61 @@ def calculate_dau_and_mau(df: DataFrame) -> DataFrame:
     mau_df.explain("extended")
     
     return dau_df, mau_df
+
+def calculate_sessions(df: DataFrame) -> DataFrame:
+    """
+    Calculate user sessions and session metrics.
+
+    Args:
+        df (DataFrame): Input DataFrame with user interactions.
+
+    Returns:
+        DataFrame: Session metrics DataFrame.
+    """
+
+    df_filtered = df.withColumn("action_end_time", from_unixtime(unix_timestamp("timestamp") + (col("duration_ms") / 1000).cast("bigint")))
+
+    window_user_time = Window.partitionBy("user_id").orderBy("timestamp")
+
+    df_with_gaps = df_filtered.withColumn("previous_action_end_time", 
+                                        lag("action_end_time").over(window_user_time)) \
+        .withColumn("gap", 
+                    when(col("previous_action_end_time").isNotNull(), 
+                        unix_timestamp("timestamp") - unix_timestamp("previous_action_end_time")) \
+                    .otherwise(0)) \
+                          .withColumn("is_new_session", when((col("gap") > 1200) | (col("gap") == 0), 1).otherwise(0))
+
+    df_session_assignments = df_with_gaps.withColumn("session_id", 
+        _sum("is_new_session").over(window_user_time.rowsBetween(Window.unboundedPreceding, Window.currentRow)))
+
+    # Calculate session durations base on duration_ms
+    df_session_durations = df_session_assignments.groupBy("user_id", "session_id") \
+        .agg(min("timestamp").alias("session_start"),
+            max("action_end_time").alias("session_end"),
+            count("action_type").alias("actions_per_session"),
+            (unix_timestamp(max("action_end_time")) - unix_timestamp(min("timestamp"))).alias("session_duration_seconds"))
+
+    # Compute session metrics with average session duration per user
+    window_user = Window.partitionBy("user_id")
+
+    df_session_metrics = df_session_durations.withColumn("avg_session_duration", 
+                                                        avg("session_duration_seconds").over(window_user)) \
+        .withColumn("session_id", concat(col("user_id"), lit("-"), col("session_id")))
+
+    # Final DataFrame with rounded metrics and additional metadata
+    session_df = df_session_metrics.select(
+        col("user_id"),
+        col("session_id"),
+        col("actions_per_session"),
+        col("avg_session_duration").alias("avg_session_duration_seconds"),
+        current_timestamp().alias("created_at"),
+        current_timestamp().alias("updated_at")
+    ).orderBy("user_id")
+
+    # Show the result
+    session_df.show()
+
+    # Show execution plan for future optimization
+    session_df.explain("extended")
+
+    return session_df
